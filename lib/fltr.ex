daf9445ec6c82efcea0adad50edfbc0b15c4f17f
Austin Schneider <me@austinschneider.com>
2026-09-02 20:32:40 -0400

    Initialize Fltr Mix library
    Create the initial Elixir library structure with formatting, tests, documentation scaffolding, and standard Mix project configuration.

---
+++ lib/fltr.ex
@@ -0,0 +1,18 @@
+defmodule Fltr do
+  @moduledoc """
+  Documentation for `Fltr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fltr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fltr_test.exs
@@ -0,0 +1,8 @@
+defmodule FltrTest do
+  use ExUnit.Case
+  doctest Fltr
+
+  test "greets the world" do
+    assert Fltr.hello() == :world
+  end
+end
